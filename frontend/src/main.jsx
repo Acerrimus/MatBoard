973ab4b3486dc7f6bf57dc8b18752eef4ef69b1c
Sahil Singh <sahilsingh.contact@gmail.com>
2026-05-02 18:33:29 +0100

    feat: rate-limit bulk-rate endpoint (30/min per IP)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import * as Sentry from '@sentry/react'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App.jsx'
 
 Sentry.init({
@@ -23,5 +24,6 @@ Sentry.init({
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
